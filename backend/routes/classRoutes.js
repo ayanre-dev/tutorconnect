f36a4dd810758958b88432be47c80eb4c66558c6
Helebele <mnaeem.bscs24seecs@seecs.edu.pk>
2025-12-17 18:01:50 +0500

    second changes in frontend, added buffer in fills

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createClass, getClassesByTutor, getAllClasses } from "../controllers/classController.js";
+import { createClass, getClassesByTutor, getAllClasses, enrollInClass } from "../controllers/classController.js";
 import { protect } from "../utils/authMiddleware.js";
 
 const router = express.Router();
--- frontend/src/polyfills.js
+++ frontend/src/polyfills.js
@@ -1,7 +1,12 @@
 // Polyfills for simple-peer (WebRTC) in Vite
+import { Buffer } from "buffer";
+
 if (typeof global === "undefined") {
     window.global = window;
 }
 if (typeof process === "undefined") {
     window.process = { env: { DEBUG: undefined }, nextTick: (cb) => setTimeout(cb, 0) };
+}
+if (typeof window.Buffer === "undefined") {
+    window.Buffer = Buffer;
 }
