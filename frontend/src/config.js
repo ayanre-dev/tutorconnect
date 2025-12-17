4cb7f71f03cc215d2e96e57009112ec50b2a2304
ayanre-dev <arehman.bscs24seecs@seecs.edu.pk>
2025-12-18 00:05:23 +0500

    just for testing backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,3 +2,6 @@
 // This assumes the backend is running on the same machine as the frontend, on port 5000.
 // If you access frontend via "192.168.x.x", it will try to hit "192.168.x.x:5000".
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || `http://${window.location.hostname}:5000`;
+
+// Debug: Log the backend URL being used
+console.log('🔗 Backend URL:', BACKEND_URL);
